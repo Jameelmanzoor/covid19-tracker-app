ebe385ce964c19bd1c19e5c644806fecd0925aef
Jameel Ahmad <66188646+Jameelmanzoor@users.noreply.github.com>
2020-09-25 22:21:17 +0500

    Added Global Stte for our app show world wide data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import GlobalStatistics from './components/GlobalStatistics';
 import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <NavBar />
+    <>
+      <NavBar />
+      <GlobalStatistics />
+    </>
   );
 }
 
