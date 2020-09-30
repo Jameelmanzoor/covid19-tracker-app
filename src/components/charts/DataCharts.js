0ec79406bf08c937fc7bd25b61f125ee4d1d873f
Jameel Ahmad <66188646+Jameelmanzoor@users.noreply.github.com>
2020-09-30 07:31:06 +0500

    Formated the document

--- src/components/charts/DataCharts.js
+++ src/components/charts/DataCharts.js
@@ -3,42 +3,59 @@ import BarCharts from './BarChart';
 import DeathsChart from './DeathsChart';
 import LineCharts from './LineChart';
 import RecoveredChart from './RecoveredChart';
+import DataTable from './DataTable';
 
 
 
-export const DataCharts = ({country}) => {
+export const DataCharts = ({ country }) => {
   const [historicalData, setHistoricalData] = useState({});
-  let url = (!country || country === 'Global')?'https://disease.sh/v3/covid-19/historical/all?lastdays=all':`https://disease.sh/v3/covid-19/historical/${country}?lastdays=all`;
+  const [barData, setBarData] = useState({});
+  const [countriesData, setcountriesData] = useState([]);
+
+  let url = (!country || country === 'Global') ? 'https://disease.sh/v3/covid-19/historical/all?lastdays=all' :
+    `https://disease.sh/v3/covid-19/historical/${country}?lastdays=all`;
+  let dataUrl = (!country || country === 'Global') ? 'https://disease.sh/v3/covid-19/historical/all?lastdays=31' :
+    `https://disease.sh/v3/covid-19/historical/${country}?lastdays=31`;
+
+
   useEffect(() => {
     async function getData() {
       const response = await fetch(url);
       const data = await response.json();
-      (!country)?setHistoricalData(data):setHistoricalData(data.timeline);;
-      
+      (!country) ? setHistoricalData(data) : setHistoricalData(data.timeline);;
+
     }
     getData();
 
   }, [url, country])
-  
 
-  const [barData, setBarData] = useState({});
-  let dataUrl = (!country || country === 'Global')?'https://disease.sh/v3/covid-19/historical/all?lastdays=31':`https://disease.sh/v3/covid-19/historical/${country}?lastdays=31`;
   useEffect(() => {
     async function getData() {
       const response = await fetch(dataUrl);
       const data = await response.json();
-      (!country)?setBarData(data):setBarData(data.timeline);
-      
+      (!country) ? setBarData(data) : setBarData(data.timeline);
+
     }
     getData();
 
   }, [dataUrl, country])
+
+  useEffect(() => {
+    async function getData() {
+      const response = await fetch('https://disease.sh/v3/covid-19/countries');
+      const data = response.json();
+      setcountriesData(data)
+    }
+    getData()
+  }, [])
+  
   return (
     <div>
       <LineCharts historicalData={historicalData} />
-      <BarCharts dataThirtyDays={barData}/>
-      <DeathsChart dataThirtyDays={barData}/>
+      <BarCharts dataThirtyDays={barData} />
+      <DeathsChart dataThirtyDays={barData} />
       <RecoveredChart dataThirtyDays={barData} />
+      <DataTable countriesData={countriesData}/>
     </div>
   )
 }
